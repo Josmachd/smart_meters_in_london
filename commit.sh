d45f2064a1ae2d590d0821cd889c875da2b66a04
Josmachd <jose.pedro.machado.work@gmail.com>
2022-04-16 17:14:38 +0100

    commit scrip added

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf __pycache__
+rm -rf .idea
+rm -rf *~
+
+#commit_message="USER INPUT"
+echo -n "Commit message: "
+read commit_message
+git add notebook.ipynb README.md commit.sh
+git commit -m"${commit_message}"
+git push
+echo $commit_message
